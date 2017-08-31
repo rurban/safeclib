e645dff99a2597fdd42e4e635731d68c43e293b4
Reini Urban <rurban@cpan.org>
2017-08-31 14:01:54 +0200

    extend smoke.sh for -ansi and c95/AMD1

--- build-tools/smoke.sh
+++ build-tools/smoke.sh
@@ -5,7 +5,11 @@ Darwin)
 
 CC="clang-mp-3.9 -fsanitize=address" ./configure --enable-debug && \
     gmake -s -j4 check || (cat tests/test-suite.log; exit)
-CC="clang-mp-4.0" ./configure && \
+CC="clang-mp-4.0 -std=c99" ./configure && \
+    gmake -s -j4 check || (cat tests/test-suite.log; exit)
+CC="gcc-mp-4.3 -ansi" ./configure && \
+    gmake -s -j4 check || (cat tests/test-suite.log; exit)
+CC="gcc-mp-4.3 -std=iso9899:199409" ./configure && \
     gmake -s -j4 check || (cat tests/test-suite.log; exit)
 CC="gcc-mp-6" ./configure && \
     gmake -s -j4 check || (cat tests/test-suite.log; exit)
@@ -23,11 +27,17 @@ Linux)
 
 CC="clang-3.9 -fsanitize=address" ./configure --enable-debug && \
     make -s -j4 check || (cat tests/test-suite.log; exit)
-CC="clang-4.0" ./configure && \
+CC="clang-4.0 -std=c99" ./configure && \
+    make -s -j4 check || (cat tests/test-suite.log; exit)
+CC="gcc-4.4 -ansi" ./configure && \
+    make -s -j4 check || (cat tests/test-suite.log; exit)
+CC="gcc-4.4 -std=iso9899:199409" ./configure && \
     make -s -j4 check || (cat tests/test-suite.log; exit)
 CC="g++-6 -std=c++11" ./configure && \
     make -s -j4 check || (cat tests/test-suite.log; exit)
 CC="gcc-6" ./configure && \
+    make -s -j4 check || (cat tests/test-suite.log; exit)
+CC="gcc-7" ./configure && \
     make -s -j4 check || (cat tests/test-suite.log; exit)
 CC="c++" ./configure && \
     make -s -j4 check || (cat tests/test-suite.log; exit)
