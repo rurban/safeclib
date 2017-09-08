0a21b3835701a2eaabae209bd08b9ffe7998251c
Reini Urban <rurban@cpan.org>
2017-09-08 17:15:39 +0200

    add qsort_s
    since compar needs a 3rd arg, we took the MIT licensed qsort from musl.
    Copyright (C) 2011 by Valentin Ochs
    
    Smoothsort, an adaptive variant of Heapsort.  Memory usage: O(1).
    Run time: Worst case O(n log n), close to O(n) in the mostly-sorted case.
    
    Note that is not yet C++ safe

--- include/safe_lib.h
+++ include/safe_lib.h
@@ -148,14 +148,12 @@ getenv_s(size_t *restrict len,
 EXTERN void *
 bsearch_s(const void *key, const void *base,
           rsize_t nmemb, rsize_t size,
-          int (*compar)(const void *k, const void *y,
-                        void *context),
+          int (*compar)(const void *k, const void *y, void *context),
           void *context);
 
 EXTERN errno_t
 qsort_s(void *base, rsize_t nmemb, rsize_t size,
-        int (*compar)(const void *x, const void *y,
-                      void *context),
+        int (*compar)(const void *x, const void *y, void *context),
         void *context);
 
 #ifdef __cplusplus
---
+++ tests/test_qsort_s.c
@@ -0,0 +1,91 @@
+/*------------------------------------------------------------------
+ * test_qsort_s
+ *
+ *------------------------------------------------------------------
+ */
+
+#include "test_private.h"
+#include "safe_lib.h"
+#include <stdlib.h>
+
+#define LEN   10
+/* needs to be sorted! */
+struct items {
+    int iv;
+    const char *sv;
+} array[] = {
+    { 1, "one" },
+    { 3, "three" },
+    { 8, "eight" },
+    { 4, "four" },
+    { 0, "zero" },
+    { 5, "five" },
+    { 2, "two" },
+    { 6, "six" },
+    { 9, "nine" },
+    { 7, "seven" },
+    { 10, "ten" }
+};
+
+static int
+comp (const void *ptr1, const void *ptr2, void *ctx)
+{
+    struct items *i1 = (struct items *) ptr1;
+    struct items *i2 = (struct items *) ptr2;
+    if (ctx) {
+        errno = -1;
+        return 0; /* shortcut just for testing */
+    }
+    return i1->iv > i2->iv ? 1 : i1->iv == i2->iv ? 0 : -1;
+}
+
+int test_qsort_s (void)
+{
+    errno_t rc;
+    int errs = 0;
+    int ind;
+
+/*--------------------------------------------------*/
+
+    rc = qsort_s(NULL, LEN, sizeof(array[0]), comp, NULL);
+    ERR(ESNULLP);
+
+    rc = qsort_s(array, LEN, sizeof(array[0]), NULL, NULL);
+    ERR(ESNULLP);
+
+/*--------------------------------------------------*/
+
+    rc = qsort_s(array, RSIZE_MAX_MEM+1, sizeof(array[0]), comp, NULL);
+    ERR(ESLEMAX);
+
+    rc = qsort_s(array, LEN, RSIZE_MAX_MEM+1, comp, NULL);
+    ERR(ESLEMAX);
+
+/*--------------------------------------------------*/
+
+    /* allow empty array */
+    rc = qsort_s(NULL, 0, 0, comp, NULL);
+    ERR(EOK);
+
+/*--------------------------------------------------*/
+
+    /* sorted */
+    rc = qsort_s(array, LEN, sizeof(array[0]), comp, NULL);
+    ERR(EOK);
+    for (ind = 0; ind < LEN; ++ind) {
+        INDCMP(!= array[ind].iv);
+    }
+
+/*--------------------------------------------------*/
+
+    return (errs);
+}
+
+#ifndef __KERNEL__
+/* simple hack to get this to work for both userspace and Linux kernel,
+   until a better solution can be created. */
+int main (void)
+{
+    return (test_qsort_s());
+}
+#endif
