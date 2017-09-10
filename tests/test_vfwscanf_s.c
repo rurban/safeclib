3b4a9a767dff0da50472d20d54d963ae67d97bb3
Reini Urban <rurban@cpan.org>
2017-09-10 09:42:23 +0200

    add test_vfwscanf_s: 0.00% to 100.00% of 21

--- tests/test_vfwscanf_s.c
+++ tests/test_vfwscanf_s.c
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------
- * test_vswscanf_s
- * File 'wchar/vswscanf_s.c'
+ * test_vfwscanf_s
+ * File 'wchar/vfwscanf_s.c'
  * Lines executed:100.00% of 21
  *
  *------------------------------------------------------------------
@@ -8,25 +8,39 @@
 
 #include "test_private.h"
 #include "safe_str_lib.h"
+#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 #define LEN   ( 128 )
 
 static wchar_t   wstr1[LEN];
 static wchar_t   wstr2[LEN];
 static char      str3[LEN];
+#define TMP   "tmpvfwscanf"
+static FILE* stream = NULL;
 
-static int vtwscanf_s (wchar_t *restrict dest, const wchar_t *restrict fmt, ...)
+static void stuff_stream(wchar_t *restrict dest)
+{
+    if (!stream)
+        stream = fopen(TMP, "w+");
+    else
+        rewind(stream);
+    fwprintf(stream, L"%ls\n", dest);
+    rewind(stream);
+}
+
+static int vtwscanf_s (FILE *stream, const wchar_t *restrict fmt, ...)
 {
     int rc;
     va_list ap;
     va_start(ap, fmt);
-    rc = vswscanf_s(dest, fmt, ap);
+    rc = vfwscanf_s(stream, fmt, ap);
     va_end(ap);
     return rc;
 }
 
-static int test_vswscanf_s (void)
+static int test_vfwscanf_s (void)
 {
     errno_t rc;
     int32_t  ind;
@@ -34,44 +48,63 @@ static int test_vswscanf_s (void)
     size_t  len2;
     size_t  len3;
     int errs = 0;
-
+    int p[2];
+    FILE *f;
+   
 /*--------------------------------------------------*/
 
-    rc = vtwscanf_s(wstr1, NULL, NULL);
-    ERREOF(ESNULLP);
+    pipe(p);
+    f = fdopen(p[0], "rb");
+    if (!f) {
+        close(p[0]);
+        close(p[1]);
+        return 0;
+    }
 
-/*--------------------------------------------------*/
-
+    write(p[1], L"a", sizeof(L"a"));
     wstr2[0] = '\0';
-    rc = vtwscanf_s(NULL, L"%ls", wstr2);
+    rc = vtwscanf_s(NULL, L"%ls", wstr2, LEN);
     ERREOF(ESNULLP);
+    WEXPSTR(wstr2, L"");
+
+    rc = vfwscanf_s(f, NULL, NULL);
+    ERREOF(ESNULLP);
+
+    /* SEGV
+      rc = vfwscanf_s(f, L"%ls", NULL);
+      ERREOF(ESNULLP);
+    */
 
 /*--------------------------------------------------*/
 
-    wcscpy(wstr1, L"      24");
-    rc = vtwscanf_s(wstr1, L"%ls %n", wstr2, LEN, &ind);
+    write(p[1], L"      24", sizeof(L"      24"));
+    rc = vtwscanf_s(f, L"%ls %n", wstr2, LEN, &ind);
     ERREOF(EINVAL);
 
-    rc = vtwscanf_s(wstr1, L"%ls %%n", wstr2);
+    stuff_stream(L"      24");
+    rc = vtwscanf_s(stream, L"%ls %%n", wstr2, LEN);
 #ifdef BSD_LIKE
     if (rc != 0) { /* BSD's return -1 on %%n */
-        printf("%s %u wrong vswscanf(\"\",L\"%%n\"): %d\n",
+        printf("%s %u wrong vfwscanf(\"\",L\"%%n\"): %d\n",
                      __FUNCTION__, __LINE__, (int)rc);
     } else
 #endif
     ERR(1);
     ERRNO(0);
 
-    rc = vtwscanf_s(wstr1, L"%ls %%n", wstr2, 6);
+    stuff_stream(L"      24");
+    rc = vtwscanf_s(stream, L"%ls %%n", wstr2, 6);
     ERR(1);
     ERRNO(0);
 
-    rc = vtwscanf_s(wstr1, L"%s %%n", str3, 6);
+    stuff_stream(L"      24");
+    rc = vtwscanf_s(stream, L"%s %%n", str3, 6);
     ERR(1);
     ERRNO(0);
     EXPSTR(str3, "24");
 
-    rc = vtwscanf_s(wstr1, L" %d", &len1);
+    stuff_stream(L"      24");
+    rc = vtwscanf_s(stream, L" %d", &len1);
     ERR(1);
     ERRNO(0);
     if ((int)len1 != 24) {
@@ -82,17 +115,11 @@ static int test_vswscanf_s (void)
 
 /*--------------------------------------------------*/
 
-    /* TODO
-    rc = vtwscanf_s(wstr1, L"%s", NULL);
-    ERR(ESNULLP)
-    */
-
-/*--------------------------------------------------*/
-
     wcscpy(wstr1, L"aaaaaaaaaa");
     len1 = wcslen(wstr1);
+    stuff_stream(wstr1);
 
-    rc = vtwscanf_s(wstr1, L"%ls", wstr2, LEN);
+    rc = vtwscanf_s(stream, L"%ls", wstr2, LEN);
     ERR(1)
     len2 = wcslen(wstr2);
     len3 = wcslen(wstr1);
@@ -108,8 +135,9 @@ static int test_vswscanf_s (void)
 /*--------------------------------------------------*/
 
     wcscpy(wstr1, L"keep it simple");
+    stuff_stream(wstr1);
 
-    rc = vtwscanf_s(wstr1, L"%ls", wstr2, LEN);
+    rc = vtwscanf_s(stream, L"%ls", wstr2, LEN);
     ERR(1);
     WEXPSTR(wstr1, L"keep it simple")
 
@@ -117,26 +145,29 @@ static int test_vswscanf_s (void)
 
     wstr1[0] = '\0';
     wstr2[0] = '\0';
+    stuff_stream(wstr1);
 
-    rc = vtwscanf_s(wstr1, L"%ls", wstr2, LEN);
-    ERR(-1)
+    rc = vtwscanf_s(stream, L"%ls", wstr2, LEN);
+    ERR(1)
     WEXPNULL(wstr1)
 
 /*--------------------------------------------------*/
 
     wstr1[0] = '\0';
     wcscpy(wstr2, L"keep it simple");
+    stuff_stream(wstr1);
 
-    rc = vtwscanf_s(wstr1, L"%ls", wstr2, LEN);
-    ERR(-1)
+    rc = vtwscanf_s(stream, L"%ls", wstr2, LEN);
+    ERR(1)
     WEXPSTR(wstr1, L"")
 
 /*--------------------------------------------------*/
 
     wcscpy(wstr1, L"qqweqq");
     wcscpy(wstr2, L"keep it simple");
+    stuff_stream(wstr1);
 
-    rc = vtwscanf_s(wstr1, L"%ls", wstr2);
+    rc = vtwscanf_s(stream, L"%ls", wstr2);
     NOERR()
     WEXPSTR(wstr1, wstr2);
 
@@ -146,19 +177,44 @@ static int test_vswscanf_s (void)
     /*
     wcscpy(wstr1, L"12345678901234567890");
 
-    rc = vtwscanf_s(wstr1, L"%ls", &wstr1[7]);
+    rc = vtwscanf_s(stream, L"%ls", &wstr1[7]);
     ERR(1);
     WEXPSTR(wstr1, L"123456712345678901234567890");
 
     wcscpy(wstr1, L"123456789");
 
-    rc = vtwscanf_s(wstr1, L"%ls", &wstr1[8]);
+    rc = vtwscanf_s(stream, L"%ls", &wstr1[8]);
     ERR(1);
     WEXPSTR(wstr1, L"12345678123456789");
     */
 
 /*--------------------------------------------------*/
 
+    fclose(stream);
+
+    wcscpy(wstr1, L"qqweqq");
+    wcscpy(wstr2, L"1");
+    stuff_stream(wstr1);
+
+    rc = vtwscanf_s(stream, L"%ls", wstr2, LEN);
+#if defined(__GLIBC__)
+    if (rc < 0) {
+        ERR(-1);
+        ERRNO(EBADF);
+    } else {
+        ERR(14); /* older glibc upstream bug */
+        ERRNO(0);
+    }
+#else
+    ERR(-1);
+#endif
+    /*WEXPNULL(wstr2); TODO zero the output args */
+
+/*--------------------------------------------------*/
+    
+    close(p[1]);
+    unlink(TMP);
+    
     return (errs);
 }
 
@@ -167,6 +223,6 @@ static int test_vswscanf_s (void)
    until a better solution can be created. */
 int main (void)
 {
-    return (test_vswscanf_s());
+    return (test_vfwscanf_s());
 }
 #endif
