a4c3d844aaa0710f97fae4f8bf45b84b23209c95
Reini Urban <rurban@cpan.org>
2017-10-05 12:20:11 +0200

    fix more docs: slen=0 allowed
    and deleted safeclib__private

--- doc/safec-3.0/search/defines_d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['wmask',['wmask',['../d8/dd8/mem__primitives__lib_8c.html#ab5deaef115ff5ca6ef1457d923d8331a',1,'mem_primitives_lib.c']]],
-  ['wsize',['wsize',['../d8/dd8/mem__primitives__lib_8c.html#a01c6b59ca21f041b23efbb9845cc4e32',1,'mem_primitives_lib.c']]]
-];
