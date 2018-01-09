e02eb4f4b16e10f256ff54b18c884ccb570b1b43
Jeremy H <jeremyhannon@users.noreply.github.com>
2018-01-09 16:04:55 -0600

    replace 'ssize_t' with 'size_t'.
    GCC provides type 'ssize_t' but IAR compiler doesn't. I don't see
    any reason why the specialized 'ssize_t' type is used. Therefore
    replace it to improve portability of the code. Use RSIZE_MAX_STR
    instead to detect negative return values.
    
    Also add curly brackets around all if-else contexts.

--- src/wchar/wcrtomb_s.c
+++ src/wchar/wcrtomb_s.c
@@ -145,17 +145,18 @@ wcrtomb_s(size_t *restrict retval,
 
     len = *retval = wcrtomb(dest, wc, ps);
 
-    if (likely((ssize_t)len > 0 && (rsize_t)len < dmax)) {
-        if (dest)
+    if (likely(len > 0 && (rsize_t)len < dmax)) {
+        if (dest) {
 #ifdef SAFECLIB_STR_NULL_SLACK
-            memset(&dest[len], 0, dmax-len);
+            memset(&dest[len], 0, dmax - len);
 #else
             dest[len] = '\0';
 #endif
+        }
         return EOK;
     } else {
         /* errno is usually EILSEQ */
-        errno_t rc = ((ssize_t)len > 0) ? ESNOSPC : errno;
+        errno_t rc = (len > 0 && len <= RSIZE_MAX_STR) ? ESNOSPC : errno;
         if (dest) {
             /* the entire src must have been copied, if not reset dest
              * to null the string. (only with SAFECLIB_STR_NULL_SLACK)
