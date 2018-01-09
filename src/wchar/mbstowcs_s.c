c976a74671ecec5d17599923de29eb439c168032
Jeremy H <jeremyhannon@users.noreply.github.com>
2018-01-09 16:04:55 -0600

    replace 'ssize_t' with 'size_t'.
    GCC provides type 'ssize_t' but IAR compiler doesn't. I don't see
    any reason why the specialized 'ssize_t' type is used. Therefore
    replace it to improve portability of the code. Use RSIZE_MAX_STR
    instead to detect negative return values.
    
    Also add curly brackets around all if-else contexts.

--- src/wchar/mbstowcs_s.c
+++ src/wchar/mbstowcs_s.c
@@ -161,7 +161,7 @@ mbstowcs_s(size_t *restrict retval,
 
     *retval = mbstowcs(dest, src, len);
 
-    if (likely((ssize_t)*retval > 0 && *retval < dmax)) {
+    if (likely(*retval > 0 && *retval < dmax)) {
         if (dest) {
 #ifdef SAFECLIB_STR_NULL_SLACK
             memset(&dest[*retval], 0, (dmax-*retval)*sizeof(wchar_t));
@@ -175,8 +175,9 @@ mbstowcs_s(size_t *restrict retval,
         if (dest) {
             size_t tmp = 0;
             errno = 0;
-            if ((ssize_t)*retval < 0) /* else ESNOSPC */
+            if (*retval > RSIZE_MAX_STR) { /* else ESNOSPC */
                 tmp = mbstowcs(NULL, src, len);
+            }
             /* with NULL either 0 or -1 is returned */
             rc = (tmp == 0) ? ESNOSPC : errno;
             /* the entire src must have been copied, if not reset dest
@@ -186,8 +187,9 @@ mbstowcs_s(size_t *restrict retval,
                               : "mbstowcs_s: illegal sequence",
                          rc);
         }
-        else
+        else {
             rc = ((size_t)*retval == 0) ? EOK : errno;
+        }
         return RCNEGATE(rc);
     }
 
