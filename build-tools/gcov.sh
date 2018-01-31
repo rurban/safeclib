90e9db1ee477fdbb43c2020b45ffa393ce3f61f5
Reini Urban <rurban@cpan.org>
2018-01-31 13:02:09 +0100

    Improve build-tools/gcov.sh
    Previously a placeholder for gcov post-process fixups,
    now the preferred configure options, to recreate the very exact
    test stats, on darwin. On linux the linenumbers and percentages
    are a bit off. It has less native helpers.

--- build-tools/gcov.sh
+++ build-tools/gcov.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-# CC=gcc-mp-6 ./configure --enable-unsafe --enable-gcov=gcov-mp-6 --disable-shared --enable-debug && gmake
-#./configure --enable-gcov --disable-shared --enable-unsafe --enable-debug && make -s -j4 gcov
+case `uname` in
+Darwin) # darwin is preferred
 
+    CC=gcc-mp-6 ./configure --enable-gcov=gcov-mp-6 --disable-shared --enable-unsafe \
+      --enable-norm-compat && \
+        gmake -s -j4 gcov ;;
+
+Linux)
+
+    echo "The repo has the stats from darwin" # expect minor differences
+
+    ./configure --enable-gcov --disable-shared --enable-unsafe --enable-norm-compat && \
+        make -s -j4 gcov ;;
+
+esac
+
+# now all done via make gcov
 # fixup and post-process .gcov
-
-perl -pi -e's{Source:(\w+)/}{Source:}' src/*/*.gcov && \
-    gcov2perl src/*/*.gcov && \
-    cover -no-gcov
+#perl -pi -e's{Source:(\w+)/}{Source:}' src/*/*.gcov && \
+#    gcov2perl src/*/*.gcov && \
+#    cover -no-gcov
