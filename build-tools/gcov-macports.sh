e59fc327e3b7318a1fd9935bd2d01d8704920264
Reini Urban <rurban@cpan.org>
2018-02-01 17:53:52 +0100

    rm build-tools/gcov-macports.sh
    superceded by build-tools/gcov.sh via case `uname`

--- build-tools/gcov-macports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CC=gcc-mp-6 ./configure --enable-gcov=gcov-mp-6 --disable-shared \
-                        --enable-debug --enable-unsafe --enable-norm-compat $@ \
-    && gmake all \
-    && gmake gcov
-
-#build-tools/gcov-upd.pl cov.lst
