055fedd904fdb93340e052d74c1af90c8c3272b1
Reini Urban <rurban@cpan.org>
2018-02-15 10:12:07 +0100

    memcmp_s: rename smax back to slen
    for consistency

--- src/extmem/memcmp_s.c
+++ src/extmem/memcmp_s.c
@@ -53,7 +53,7 @@
  * @param  dest   pointer to memory to compare against
  * @param  src    pointer to the source memory to compare with dest
  * @param  dmax   maximum length of dest, in bytes
- * @param  smax   length of the source memory block
+ * @param  slen   length of the source memory block
  * @param  *diff  pointer to the diff which is an integer greater
  *                than, equal to or less than zero according to
  *                whether the object pointed to by dest is
@@ -61,16 +61,16 @@
  *                pointed to by src.
  *
  * @pre   Neither dest nor src shall be a null pointer.
- * @pre   Neither dmax nor smax shall be 0.
+ * @pre   Neither dmax nor slen shall be 0.
  * @pre   dmax shall not be greater than RSIZE_MAX_MEM or sizeof(dest)
- * @pre   smax shall not be greater than dmax or sizeof(src)
+ * @pre   slen shall not be greater than dmax or sizeof(src)
  *
  * @retval  EOK         when operation is successful
  * @retval  ESNULLP     when dst/src is NULL POINTER
- * @retval  ESZEROL     when dmax/smax = ZERO
- * @retval  ESLEMAX     when dmax/smax > RSIZE_MAX_MEM or > sizeof(dest/src)
- * @retval  ESLEWRNG    when dmax/smax != sizeof(dest/src) and --enable-error-dmax
- * @retval  ESNOSPC     when dmax < smax
+ * @retval  ESZEROL     when dmax/slen = ZERO
+ * @retval  ESLEMAX     when dmax/slen > RSIZE_MAX_MEM or > sizeof(dest/src)
+ * @retval  ESLEWRNG    when dmax/slen != sizeof(dest/src) and --enable-error-dmax
+ * @retval  ESNOSPC     when dmax < slen
  *
  * @see
  *    memcmp16_s(), memcmp32_s()
@@ -79,7 +79,7 @@
 
 EXPORT errno_t
 _memcmp_s_chk (const void *dest, rsize_t dmax,
-               const void *src,  rsize_t smax, int *diff,
+               const void *src,  rsize_t slen, int *diff,
                const size_t destbos, const size_t srcbos)
 {
     const uint8_t *dp;
@@ -136,34 +136,34 @@ _memcmp_s_chk (const void *dest, rsize_t dmax,
 #endif
     }
 
-    if (unlikely(smax == 0)) {
-        invoke_safe_mem_constraint_handler("memcmp_s: smax is 0",
+    if (unlikely(slen == 0)) {
+        invoke_safe_mem_constraint_handler("memcmp_s: slen is 0",
                    NULL, ESZEROL);
         return (RCNEGATE(ESZEROL));
     }
 
-    if (unlikely(smax > dmax)) {
-        errno_t error = smax > RSIZE_MAX_MEM ? ESLEMAX : ESNOSPC;
-        invoke_safe_mem_constraint_handler("memcmp_s: smax exceeds dmax",
+    if (unlikely(slen > dmax)) {
+        errno_t error = slen > RSIZE_MAX_MEM ? ESLEMAX : ESNOSPC;
+        invoke_safe_mem_constraint_handler("memcmp_s: slen exceeds dmax",
                        (void*)dest, error);
         return (RCNEGATE(error));
     }
 
     if (srcbos == BOS_UNKNOWN) {
-        if (unlikely(smax > RSIZE_MAX_MEM)) {
-            invoke_safe_mem_constraint_handler("memcmp_s: smax exceeds max",
+        if (unlikely(slen > RSIZE_MAX_MEM)) {
+            invoke_safe_mem_constraint_handler("memcmp_s: slen exceeds max",
                        (void*)src, ESLEMAX);
             return (RCNEGATE(ESLEMAX));
         }
     } else {
-        if (unlikely(smax > srcbos)) {
-            invoke_safe_mem_constraint_handler("memcmp_s: smax exceeds src",
+        if (unlikely(slen > srcbos)) {
+            invoke_safe_mem_constraint_handler("memcmp_s: slen exceeds src",
                        (void*)src, ESLEMAX);
             return (RCNEGATE(ESLEMAX));
         }
 #ifdef HAVE_WARN_DMAX
-        if (unlikely(smax != srcbos)) {
-            handle_mem_bos_chk_warn("memcmp_s", (void*)src, smax, srcbos);
+        if (unlikely(slen != srcbos)) {
+            handle_mem_bos_chk_warn("memcmp_s",  (void*)src, slen, srcbos);
 # ifdef HAVE_ERROR_DMAX
             return (RCNEGATE(ESLEWRNG));
 # endif
@@ -179,7 +179,7 @@ _memcmp_s_chk (const void *dest, rsize_t dmax,
 
     /* now compare sp to dp */
     *diff = 0;
-    while (dmax > 0 && smax > 0) {
+    while (dmax > 0 && slen > 0) {
         if (*dp != *sp) {
             /***  *diff = *dp - *sp; ***/
             *diff = *dp < *sp ? -1 : 1;
@@ -187,7 +187,7 @@ _memcmp_s_chk (const void *dest, rsize_t dmax,
         }
 
         dmax--;
-        smax--;
+        slen--;
 
         dp++;
         sp++;
