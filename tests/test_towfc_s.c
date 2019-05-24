e047e2ac587feb22e9216e31a9e6e741b502178a
Reini Urban <rurban@cpan.org>
2019-05-24 17:53:27 +0200

    bump to version 3.5

--- tests/test_towfc_s.c
+++ tests/test_towfc_s.c
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------
  * test_towfc_s.c
  * File 'towfc_s.c'
- * Lines executed:99.19% of 123
+ * Lines executed:98.44% of 128
  *
  * Full case-folding regarding latest Unicode (10.0) CaseFolding.txt
  * Some F characters fold to multiples.
