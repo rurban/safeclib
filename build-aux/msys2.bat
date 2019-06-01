74230d93a69eb73f686c91434715fdfa616e0783
Reini Urban <rurban@cpan.org>
2019-06-01 20:09:35 +0200

    mingw: skip msys2 update to 7.0
    there are still inconsistencies vs a clean install.

--- build-aux/msys2.bat
+++ build-aux/msys2.bat
@@ -18,13 +18,13 @@ SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
 
 echo Updating pacman...
 bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
-bash -lc "pacman -Syyu --noconfirm"
+rem bash -lc "pacman -Syyu --noconfirm"
 
 rem echo Installing git...
 rem bash -lc "pacman -S --needed --noconfirm git"
 
 echo Installing common dependencies...
 rem mingw-w64-x86_64-pdcurses and mingw-w64-x86_64-ncurses are in conflict...
-bash -lc "pacman -Rddn --noconfirm mingw-w64-%MSYS2_ARCH%-ncurses mingw-w64-%MSYS2_ARCH%-termcap mingw-w64-i686-gcc-ada mingw-w64-i686-gcc-objc mingw-w64-x86_64-gcc-ada mingw-w64-x86_64-gcc-objc"
+rem bash -lc "pacman -Rddn --noconfirm mingw-w64-%MSYS2_ARCH%-ncurses mingw-w64-%MSYS2_ARCH%-termcap mingw-w64-i686-gcc-ada mingw-w64-i686-gcc-objc mingw-w64-x86_64-gcc-ada mingw-w64-x86_64-gcc-objc"
 bash -lc "pacman -Syyu --needed --noconfirm mingw-w64-%MSYS2_ARCH%-toolchain mingw-w64-%MSYS2_ARCH%-libtool mingw-w64-%MSYS2_ARCH%-libiconv"
 rem autoconf-2.69 automake-wrapper
