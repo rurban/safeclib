3dda7cbff2cbd9693f5c6085ff0442b781c8810d
Reini Urban <rurban@cpan.org>
2020-08-18 08:24:19 +0200

    appveyor: Temporary keyring mess
    See https://www.msys2.org/news/
    From libredwg

--- build-aux/msys2.bat
+++ build-aux/msys2.bat
@@ -16,14 +16,24 @@ echo Installation MSYS2 build preconditions...
 echo Extending path to MSYS...
 SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
 
+echo Temporary keyring mess (https://www.msys2.org/news/)
+bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
+bash -lc "pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz{.sig,}"
+bash -lc "pacman -U --noconfirm msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+rem bash -lc "pacman -U --config <(echo) msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+
 echo Updating pacman...
+bash -lc "pacman -Syy --noconfirm pacman"
 bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
-rem bash -lc "pacman -Syyu --noconfirm"
+bash -lc "pacman -Syyu --noconfirm"
+rem need it twice, really
+bash -lc "pacman -Syyu --noconfirm"
 
 rem echo Installing git...
 rem bash -lc "pacman -S --needed --noconfirm git"
 
-echo Installing common dependencies...
+echo Installing dependencies...
 rem mingw-w64-x86_64-pdcurses and mingw-w64-x86_64-ncurses are in conflict...
 rem bash -lc "pacman -Rddn --noconfirm mingw-w64-%MSYS2_ARCH%-ncurses mingw-w64-%MSYS2_ARCH%-termcap mingw-w64-i686-gcc-ada mingw-w64-i686-gcc-objc mingw-w64-x86_64-gcc-ada mingw-w64-x86_64-gcc-objc"
 bash -lc "pacman -Syyu --needed --noconfirm mingw-w64-%MSYS2_ARCH%-toolchain mingw-w64-%MSYS2_ARCH%-libtool mingw-w64-%MSYS2_ARCH%-libiconv"
