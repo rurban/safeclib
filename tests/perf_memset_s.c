919d494a892bbba3730bdfd50e0543ae74b71e13
Reini Urban <rurban@cpan.org>
2020-09-09 11:37:44 +0200

    improve perf_memset_s: proper timer linearization
    as in smhasher.
    also change the percent to compare against. mem_prim_set vs memset
    
    gcc now as fast as memset, only clang 2x slower without -march=native.
    With -march=native gcc is 2.5% faster than glibc, clang 3% slower.

--- tests/perf_memset_s.c
+++ tests/perf_memset_s.c
@@ -19,8 +19,8 @@
  *   Speed overhead: -1 - 2%
  *
  * fc32 on ryzen3:
- * gcc-10.2.1 speed overhead: 56.56%
- * clang-10.0 speed overhead: 51.25%
+ * gcc-10.2.1 speed overhead: 5.13%
+ * clang-10.0 speed overhead: 51.16%
  *
  */
 
@@ -54,6 +54,8 @@ uint8_t mem1[LEN];
 uint8_t mem2[LEN];
 
 static inline clock_t rdtsc();
+static inline uint64_t timer_start();
+static inline uint64_t timer_end();
 static double timing_loop(uint32_t len, uint32_t loops);
 int main(void);
 
@@ -72,12 +74,80 @@ static inline clock_t rdtsc() {
 #endif
 }
 
+// see https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
+// 3.2.1 The Improved Benchmarking Method
+static inline uint64_t timer_start()
+{
+#if defined (__i386__) || (defined(__x86_64__) && SIZEOF_SIZE_T == 4)
+  uint32_t cycles_high, cycles_low;
+  __asm__ volatile
+      ("cpuid\n\t"
+       "rdtsc\n\t"
+       "mov %%edx, %0\n\t"
+       "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+       "%eax", "%ebx", "%ecx", "%edx");
+    return ((uint64_t)cycles_high << 32) | cycles_low;
+#elif defined __x86_64__
+  uint32_t cycles_high, cycles_low;
+  __asm__ volatile
+      ("cpuid\n\t"
+       "rdtsc\n\t"
+       "mov %%edx, %0\n\t"
+       "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+       "%rax", "%rbx", "%rcx", "%rdx");
+  return ((uint64_t)cycles_high << 32) | cycles_low;
+#elif defined(__ARM_ARCH) && (__ARM_ARCH >= 6)
+  // V6 is the earliest arch that has a standard cyclecount
+  uint32_t pmccntr;
+  uint32_t pmuseren;
+  uint32_t pmcntenset;
+  // Read the user mode perf monitor counter access permissions.
+  asm volatile("mrc p15, 0, %0, c9, c14, 0" : "=r"(pmuseren));
+  if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
+    asm volatile("mrc p15, 0, %0, c9, c12, 1" : "=r"(pmcntenset));
+    if (pmcntenset & 0x80000000ul) {  // Is it counting?
+      asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
+      // The counter is set up to count every 64th cycle
+      return static_cast<int64_t>(pmccntr) * 64;  // Should optimize to << 6
+    }
+  }
+  return (uint64_t)rdtsc();
+#else
+  return (uint64_t)rdtsc();
+#endif
+}
+
+static inline uint64_t timer_end()
+{
+#if defined (__i386__) || (defined(__x86_64__) && defined (HAVE_BIT32))
+  uint32_t cycles_high, cycles_low;
+  __asm__ volatile
+      ("rdtscp\n\t"
+       "mov %%edx, %0\n\t"
+       "mov %%eax, %1\n\t"
+       "cpuid\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+       "%eax", "%ebx", "%ecx", "%edx");
+    return ((uint64_t)cycles_high << 32) | cycles_low;
+#elif defined __x86_64__
+  uint32_t cycles_high, cycles_low;
+  __asm__ volatile
+      ("rdtscp\n\t"
+       "mov %%edx, %0\n\t"
+       "mov %%eax, %1\n\t"
+       "cpuid\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+       "%rax", "%rbx", "%rcx", "%rdx");
+  return ((uint64_t)cycles_high << 32) | cycles_low;
+#else
+  return (uint64_t)rdtsc();
+#endif
+}
+
 static double timing_loop(uint32_t len, uint32_t loops) {
     uint32_t i;
     size_t errors = 0;
 
-    clock_t clock_start;
-    clock_t clock_end;
+    uint64_t clock_start;
+    uint64_t clock_end;
 
     clock_t sl_clock_diff;
     clock_t sd_clock_diff;
@@ -98,24 +168,24 @@ static double timing_loop(uint32_t len, uint32_t loops) {
     /*
      * Fast safe C Lib Routine (without MEMORY_BARRIER)
      */
-    clock_start = rdtsc();
+    clock_start = timer_start();
     for (i = 0; i < loops; i++) {
         mem_prim_set(mem1, len, 0);
     }
-    clock_end = rdtsc();
+    clock_end = timer_end();
 
     sp_clock_diff = (clock_end - clock_start) / loops;
 
     /*
      * Slow safe C Lib Routine (with MEMORY_BARRIER)
      */
-    clock_start = rdtsc();
+    clock_start = timer_start();
     for (i = 0; i < loops; i++) {
         volatile errno_t rc;
         rc = memset_s(mem1, len, 0, len);
         errors += rc;
     }
-    clock_end = rdtsc();
+    clock_end = timer_end();
 
     /*
      * Note that background stuff continues to run, i.e. interrupts, so
@@ -126,13 +196,13 @@ static double timing_loop(uint32_t len, uint32_t loops) {
     /*
      * Standard C Routine
      */
-    clock_start = rdtsc();
+    clock_start = timer_start();
     for (i = 0; i < loops; i++) {
         volatile char *rc;
         rc = (char *)memset(mem1, 0, len);
         errors += *rc;
     }
-    clock_end = rdtsc();
+    clock_end = timer_end();
 
     /*
      * Note that background stuff continues to run, i.e. interrupts, so
@@ -144,7 +214,7 @@ static double timing_loop(uint32_t len, uint32_t loops) {
     sp_clock_dur = ((double)(sp_clock_diff) / CLOCKS_PER_SEC);
     sl_clock_dur = ((double)(sl_clock_diff) / CLOCKS_PER_SEC);
     sd_clock_dur = ((double)(sd_clock_diff) / CLOCKS_PER_SEC);
-    percent = 100 * (sl_clock_dur - sd_clock_dur) / sl_clock_dur;
+    percent = 100 * (sp_clock_dur - sd_clock_dur) / sp_clock_dur;
 
     /* just to disable optimizing away the inner loop */
     /* fprintf(stderr, "errors %lu\n", errors); */
