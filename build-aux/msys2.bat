8c01292fca989adc82efc0cffeb14bc4ae8d929d
Reini Urban <rurban@cpan.org>
2020-10-07 10:12:21 +0200

    msys2: wipe and re-init keyrings
    repo.msys2.org times out

--- build-aux/msys2.bat
+++ build-aux/msys2.bat
@@ -17,11 +17,14 @@ echo Extending path to MSYS...
 SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
 
 echo Temporary keyring mess (https://www.msys2.org/news/)
-bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
-bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
-bash -lc "pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
-bash -lc "pacman -U --noconfirm msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
-bash -lc "pacman -U --config <(echo) msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+rem bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+rem bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
+rem bash -lc "pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
+rem bash -lc "pacman -U --noconfirm msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+rem bash -lc "pacman -U --config <(echo) msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+bash -lc "rm -r /etc/pacman.d/gnupg/"
+bash -lc "pacman-key --init"
+bash -lc "pacman-key --populate msys2"
 
 echo Updating pacman...
 bash -lc "pacman -Syy --noconfirm pacman"
