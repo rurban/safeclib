bd6c2a8f33d7f234c4074d15462ee6760097b4b9
Reini Urban <rurban@cpan.org>
2021-03-19 09:05:45 +0100

    rename u8glen_s to u8width_s
    as in libunistring

--- tests/test_u8width_s.c
+++ tests/test_u8width_s.c
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------
- * test_u8glen_s
- * File 'extu8/u8glen_s.c'
+ * test_u8width_s
+ * File 'extu8/u8width_s.c'
  * Lines executed:94.12% of 17
  *
  *------------------------------------------------------------------
@@ -12,7 +12,7 @@
 #define LEN (128)
 
 static char8_t str1[LEN];
-int test_u8glen_s(void);
+int test_u8width_s(void);
 
 #define EXPLEN(n)                                                              \
     if (len != n) {                                                            \
@@ -27,7 +27,7 @@ int test_u8glen_s(void);
         errs++;                                                                \
     }
 
-int test_u8glen_s(void) {
+int test_u8width_s(void) {
     rsize_t len;
     rsize_t std_len;
     rsize_t max_len;
@@ -38,49 +38,49 @@ int test_u8glen_s(void) {
 #ifndef HAVE_CT_BOS_OVR
     max_len = 3;
     EXPECT_BOS("empty str")
-    len = u8glen_s(NULL, max_len);
+    len = u8width_s(NULL, max_len);
     EXPLEN(0)
     /*--------------------------------------------------*/
 
     max_len = 0;
     EXPECT_BOS("empty str or smax")
-    len = u8glen_s("test", 0);
+    len = u8width_s("test", 0);
     EXPLEN(0)
     /*--------------------------------------------------*/
 
 #ifdef HAVE___BUILTIN_OBJECT_SIZE
     strcpy(str1, "test");
     EXPECT_BOS("str overflow")
-    len = u8glen_s(str1, LEN + 1);
+    len = u8width_s(str1, LEN + 1);
     EXPLEN(4)
 #endif
 
     strcpy(str1, "test");
     max_len = RSIZE_MAX_STR + 1;
     EXPECT_BOS("str overflow")
-    len = u8glen_s(str1, max_len);
+    len = u8width_s(str1, max_len);
     EXPLEN(0)
 
     /* overflow: sizeof = 5 */
     /* PS: compile-time check once fixed by adding const'ness and all warnings
      */
     EXPECT_BOS("str overflow")
-    len = u8glen_s("test", 6);
+    len = u8width_s("test", 6);
     EXPLEN(4)
     EXPECT_BOS("str overflow")
-    len = u8glen_s("test", 7);
+    len = u8width_s("test", 7);
     EXPLEN(4)
     EXPECT_BOS("str overflow")
-    len = u8glen_s("test", 21);
+    len = u8width_s("test", 21);
     EXPLEN(4)
 
 #endif
 
     /* no overflow: sizeof = 5 */
-    len = u8glen_s("test", 4);
+    len = u8width_s("test", 4);
     EXPLEN(4)
     /* no overflow: sizeof = 5 */
-    len = u8glen_s("test", 5);
+    len = u8width_s("test", 5);
     EXPLEN(4)
 
     //return errs;
@@ -90,7 +90,7 @@ int test_u8glen_s(void) {
     strcpy(str1, "");
     max_len = LEN;
 
-    len = u8glen_s(str1, LEN);
+    len = u8width_s(str1, LEN);
     STDLEN()
 
     /*--------------------------------------------------*/
@@ -99,10 +99,10 @@ int test_u8glen_s(void) {
     strcpy(str1, "t");
     max_len = LEN;
 
-    len = u8glen_s(str1, max_len);
+    len = u8width_s(str1, max_len);
     STDLEN()
 
-    len = u8glen_s("t", 1); /* static string */
+    len = u8width_s("t", 1); /* static string */
     STDLEN()
 
     /*--------------------------------------------------*/
@@ -111,7 +111,7 @@ int test_u8glen_s(void) {
     strcpy(str1, "to");
     max_len = LEN;
 
-    len = u8glen_s(str1, max_len);
+    len = u8width_s(str1, max_len);
     STDLEN()
 
     /*--------------------------------------------------*/
@@ -120,34 +120,34 @@ int test_u8glen_s(void) {
     strcpy(str1, "testing");
     max_len = LEN;
 
-    len = u8glen_s(str1, max_len);
+    len = u8width_s(str1, max_len);
     STDLEN()
 
     /*--------------------------------------------------*/
 
     max_len = 1;
-    len = u8glen_s("testing", 1);
+    len = u8width_s("testing", 1);
     EXPLEN(max_len)
 
     max_len = 2;
-    len = u8glen_s("testing", 2);
+    len = u8width_s("testing", 2);
     EXPLEN(max_len)
 
     max_len = 3;
-    len = u8glen_s("testing", 3);
+    len = u8width_s("testing", 3);
     EXPLEN(max_len)
 
     /*--------------------------------------------------*/
-    len = u8glen_s("test", 5);
+    len = u8width_s("test", 5);
     EXPLEN(4)
 
-    len = u8glen_s("진서/真書", LEN);
+    len = u8width_s("진서/真書", LEN);
     EXPLEN(5)
-    len = u8glen_s("암클", LEN);
+    len = u8width_s("암클", LEN);
     EXPLEN(2)
-    len = u8glen_s("Café", LEN); // TODO normalize
+    len = u8width_s("Café", LEN); // TODO normalize
     EXPLEN(4)
-    len = u8glen_s("Café", LEN);
+    len = u8width_s("Café", LEN);
     EXPLEN(4)
 
     /*--------------------------------------------------*/
@@ -155,4 +155,4 @@ int test_u8glen_s(void) {
     return (errs);
 }
 
-int main(void) { return (test_u8glen_s()); }
+int main(void) { return (test_u8width_s()); }
