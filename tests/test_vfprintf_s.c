4e525314a826cf00eee2bba0adb22dd78dc2e4a2
Reini Urban <rurban@cpan.org>
2021-11-15 17:21:21 +0100

    fix t_vfprintf_s
    The kernel caches the stat mode, fchmod fails.
    So close the file, and re-open with wrong perms.
    
    some windows variants return EINVAL with fopen w+ (not wine, but
    on Github actions Windows Server 2019), so workaround that quirks

--- tests/test_vfprintf_s.c
+++ tests/test_vfprintf_s.c
@@ -6,7 +6,6 @@
  *------------------------------------------------------------------
  */
 
-//#define _POSIX_C_SOURCE 199309
 #include "test_private.h"
 #include "safe_str_lib.h"
 #include <unistd.h>
@@ -14,11 +13,6 @@
 #include <sys/stat.h>
 #endif
 
-//#ifdef HAVE_VFPRINTF_S
-//#define HAVE_NATIVE 1
-//#else
-//#define HAVE_NATIVE 0
-//#endif
 #include "test_msvcrt.h"
 
 #define TMP "tmpvfp"
@@ -28,6 +22,7 @@ static FILE *out;
 static char buf[LEN];
 static char str1[LEN];
 static char str2[LEN];
+
 int vtfprintf_s(FILE *restrict stream, const char *restrict fmt, ...);
 int test_vfprintf_s(void);
 
@@ -40,32 +35,73 @@ int vtfprintf_s(FILE *restrict stream, const char *restrict fmt, ...) {
     return rc;
 }
 
-#define CMP_AND_RESET(out, s)                                                  \
-    pos = ftell(out);                                                          \
-    fflush(out);							       \
-    rewind(out);							       \
-    *buf = '\0';                                                               \
-    ind = fread(buf, pos, 1, out);                                             \
-    EXPSTR(buf, s)							       \
-    rewind(out);							       \
-    ind = ftruncate(fileno(out), 0L);
+int errs = 0;
+int win_wplus = 0; // if EINVAL with fopen w+
+
+// a variant for debugging
+static size_t cmp_and_reset(FILE *out, const char *s) {
+    size_t pos = ftell(out);
+    fflush(out);
+    *buf = '\0';
+    if (win_wplus) {
+        fclose(out);
+        out = fopen(TMP, "r");
+        size_t nread = fread(buf, pos, 1, out);
+        fclose(out);
+        out = fopen(TMP, "w");
+    } else {
+        rewind(out);
+        size_t nread = fread(buf, pos, 1, out);
+    }
+    EXPSTR(buf, s)
+    if (!win_wplus) {
+        rewind(out);
+        size_t nread = ftruncate(fileno(out), 0L);
+    }
+    return pos;
+}
 
 int test_vfprintf_s(void) {
     errno_t rc;
     int32_t ind;
     long pos;
-    int errs = 0;
 
+    // mingw (with some versions) disallows w+ (EINVAL)
     out = fopen(TMP, "w+");
     if (errno) {
-        perror("fopen");
-        return 1;
+        perror("fopen w+");
+        fclose(out);
+	out = fopen(TMP, "w");
+	win_wplus = 1;
     }
+
+#define CMP_AND_RESET(out, s)                                                  \
+    pos = ftell(out);                                                          \
+    fflush(out);                                                               \
+    *buf = '\0';                                                               \
+    if (win_wplus) {                                                           \
+        fclose(out);                                                           \
+        out = fopen(TMP, "r");                                                 \
+        ind = fread(buf, pos, 1, out);                                         \
+        fclose(out);                                                           \
+        out = fopen(TMP, "w");                                                 \
+    } else {                                                                   \
+        rewind(out);                                                           \
+        ind = fread(buf, pos, 1, out);                                         \
+    }                                                                          \
+    EXPSTR(buf, s)                                                             \
+    if (!win_wplus) {                                                          \
+        rewind(out);                                                           \
+        ind = ftruncate(fileno(out), 0L);                                      \
+    }
+
 #ifdef HAVE_SYS_STAT_H
     {
         int fd = fileno(out);
         if (fd < 0) {
             perror("invalid " TMP);
+            fclose(out);
+            unlink(TMP);
             return 1;
         }
     }
@@ -76,41 +112,43 @@ int test_vfprintf_s(void) {
     rc = vtfprintf_s(out, NULL);
     init_msvcrt(rc == -ESNULLP, &use_msvcrt);
     NEGERR_MSVC(ESNULLP, EOF);
-    CMP_AND_RESET(out, "")
+    CMP_AND_RESET(out, "");
 
     rc = vtfprintf_s(NULL, "");
     NEGERR_MSVC(ESNULLP, EOF);
-    CMP_AND_RESET(out, "")
+    CMP_AND_RESET(out, "");
 
     rc = vtfprintf_s(out, "", NULL);
     NEGERR(EOK)
-    CMP_AND_RESET(out, "")
+    CMP_AND_RESET(out, "");
 
     rc = vtfprintf_s(NULL, "%s", NULL);
     NEGERR(ESNULLP);
-    CMP_AND_RESET(out, "")
+    CMP_AND_RESET(out, "");
 
     /*--------------------------------------------------*/
 
     str1[0] = '\0';
     rc = vtfprintf_s(out, "%s%n\n", str1, &ind);
     NEGERR_MSVC(EINVAL, EOF);
-    CMP_AND_RESET(out, "")
+    CMP_AND_RESET(out, "");
 
     /*--------------------------------------------------*/
 
     rc = vtfprintf_s(out, "%s%%n\n", str1);
     if (rc < 0) {
         perror("vtfprintf_s");
+        fclose(out);
+        unlink(TMP);
         return errs;
     }
     ERR(3)
     //rewind(out);
-    CMP_AND_RESET(out, "%n\n") //FIXME
+    CMP_AND_RESET(out, "%n\n"); //FIXME
 
     rc = vtfprintf_s(out, "%%n\n");
     ERR(3);
-    CMP_AND_RESET(out, "%n\n")
+    CMP_AND_RESET(out, "%n\n");
 
     /*--------------------------------------------------*/
 
@@ -124,7 +162,7 @@ int test_vfprintf_s(void) {
 
     rc = vtfprintf_s(out, "%s%s", str1, str2);
     ERR(4);
-    CMP_AND_RESET(out, "1234")
+    CMP_AND_RESET(out, "1234");
 
     /*--------------------------------------------------*/
 
@@ -132,7 +170,7 @@ int test_vfprintf_s(void) {
     /* 0x7fffffff + 1 >INT_MAX */
     rc = printf_s("\n%2147483648d\n", INT_MAX);
     NEGERR(ESLEMAX);
-    CMP_AND_RESET(out, "")
+    CMP_AND_RESET(out, "");
     //#if defined(__GLIBC__)
     //ERRNO(EOVERFLOW);
     //#endif
@@ -144,49 +182,27 @@ int test_vfprintf_s(void) {
     ANYERR();
 #endif
 
-#if defined HAVE_SYS_STAT_H && 0
-    {
-        int fd = fileno(out);
-        if (fd >= 0) {
-            struct stat st;
-            fstat(fd, &st);
-            if (!S_ISREG(st.st_mode))
-                return 1;
-            /* print to not-writable stream */
-#ifndef _WIN32
-            if (fchmod(fd, st.st_mode & ~(S_IWUSR|S_IWGRP|S_IWOTH)) == 0)
-#else
-            if (chmod(TMP, st.st_mode & ~(S_IWUSR|S_IWGRP|S_IWOTH)) == 0)
-#endif
-	    {
-                struct stat st1;
-                fstat(fd, &st1);
-                if (st1.st_mode < st.st_mode) {
-                    rc = vtfprintf_s(out, "%s", str1);
-                    ANYERR(); // Linux produces EINVAL, other EBADF
-                    if (errno >= 0) {
-                        debug_printf("%s %u  Error errno=%d \n", __FUNCTION__,
-                                     __LINE__, (int)errno);
-                        errs++;
-                    }
-                    CMP_AND_RESET(out, "")
-		    if (pos) {
-			debug_printf("%s %u  pos=%ld \n", __FUNCTION__, __LINE__, pos);
-			errs++;
-		    }
-                } else {
-		    debug_printf("fchmod failed to remove write perms\n");
-		}
-            }
-#ifndef _WIN32
-            fchmod(fd, st.st_mode); // restore wr perms
-#else
-            chmod(TMP, st.st_mode);
-#endif
-            rewind(out);
-        }
+    fclose(out);
+    out = fopen(TMP, "r");
+    if (errno) {
+        perror("fopen r");
+        unlink(TMP);
+        return errs + 1;
+    }    
+    /* print to not-writable stream */
+    errno = 0;
+    rc = vtfprintf_s(out, "%s", str1);
+    ANYERR(); // Linux may produce EINVAL, most others EBADF
+    if (errno == 0) {
+        debug_printf("%s %u  Error errno=%d \n", __FUNCTION__,
+                     __LINE__, (int)errno);
+        errs++;
     }
-#endif
+    CMP_AND_RESET(out, "");
+    if (pos) {
+        debug_printf("%s %u  pos=%ld \n", __FUNCTION__, __LINE__, pos);
+        errs++;
+    }
 
     /*--------------------------------------------------*/
 
@@ -194,8 +210,13 @@ int test_vfprintf_s(void) {
 
     /* print to closed stream: across libc's unportable, and not valgrind-safe */
     rc = vtfprintf_s(out, "%s", str1);
-    ERR(-EBADF);
-    ERRNO(EBADF);
+    ANYERR(); // Windows may produce EOF, most others EBADF
+    // EBADF (linux, mingw) or EINVAL (bsd)
+    if (errno == 0) {
+        debug_printf("%s %u  Error errno=%d \n", __FUNCTION__,
+                     __LINE__, (int)errno);
+        errs++;
+    }
 
     unlink(TMP);
 
