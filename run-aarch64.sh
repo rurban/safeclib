9caa9a51b5752c720e99ae29309458249b1ef706
Reini Urban <reini.urban@gmail.com>
2024-03-09 08:47:30 +0100

    add aarch64 CI to GH actions
    we had a drone CI, but this was disabled and failed.
    add relprep and the aarch64-m1 branch.

--- run-aarch64.sh
+++ run-aarch64.sh
@@ -2,5 +2,5 @@
 # ./configure --host=aarch64-linux-gnu --disable-shared --enable-debug
 # echo to avoid libssp
 # sed -i 's,-fstack-protector-strong -fstack-clash-protection,,' Makefile */Makefile
-docker build --rm  -f Dockerfile-fedora-aarch64 -t aarch64 .
-docker run --rm -t aarch64 $@
+docker build -f Dockerfile-fedora-aarch64 -t safeclib-aarch64 .
+docker run --rm -t safeclib-aarch64 -it $@
